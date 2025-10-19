66ae3bbe052e9410bc641de8bf44eb5e001d11eb
Santy <santyrahmawatiusman@gmail.com>
2025-10-19 16:37:41 +0700

    commit ke 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,229 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CalculatorBMI());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CalculatorBMI extends StatelessWidget {
+  const CalculatorBMI({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Kalkulator BMI',
+      home: BMIScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BMIScreen extends StatefulWidget {
+  const BMIScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BMIScreen> createState() => _BMIScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BMIScreenState extends State<BMIScreen> {
+  final _berat = TextEditingController();
+  final _tinggi = TextEditingController();
+  String _jenisKelamin = 'Laki-laki';
 
-  void _incrementCounter() {
+  double? _hasil;
+  String? _kategori;
+
+  void _hitungMBI() {
+    final double weight = double.tryParse(_berat.text) ?? 0;
+    final double height = double.tryParse(_tinggi.text) ?? 0;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (weight > 0 && height > 0) {
+        final double heightInM = height / 100;
+        final bmi = weight / (heightInM * heightInM);
+        _hasil = bmi;
+        
+        if (_jenisKelamin == 'Laki-laki') {
+          if (bmi < 18.5) {
+            _kategori = "Kurus";
+          } else if (bmi < 25) {
+            _kategori = 'Ideal';
+          } else if (bmi < 30) {
+            _kategori = "Gemuk";
+          } else {
+            _kategori = "Obesitas";
+          }
+        } else {
+          if (bmi < 17) {
+            _kategori = "Kurus";
+          } else if (bmi < 23) {
+            _kategori = 'Ideal';
+          } else if (bmi < 30) {
+            _kategori = "Gemuk";
+          } else {
+            _kategori = "Obesitas";
+          }
+        }
+      } else {
+        _hasil = null;
+        _kategori = 'Masukan data yang valid';
+      }
+    });
+  }
+
+  void _resetData() {
+    setState(() {
+      _berat.clear();
+      _tinggi.clear();
+      _hasil = null;
+      _kategori = null;
+      _jenisKelamin = 'Laki-laki';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Kalkulator BMI'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Jenis Kelamin',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: RadioListTile(
+                            title: Text('Laki-laki'),
+                            value: 'Laki-laki',
+                            groupValue: _jenisKelamin,
+                            onChanged: (value) {
+                              setState(() {
+                                _jenisKelamin = value!;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile(
+                            title: Text('Perempuan'),
+                            value: 'Perempuan',
+                            groupValue: _jenisKelamin,
+                            onChanged: (value) {
+                              setState(() {
+                                _jenisKelamin = value!;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            
+            // Input Berat Badan
+            TextField(
+              controller: _berat,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Berat Badan (Kg)',
+                icon: Icon(Icons.monitor_weight),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+          
+            TextField(
+              controller: _tinggi,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Tinggi Badan (cm)',
+                icon: Icon(Icons.height),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 24),
+            
+            // Tombol Hitung dan Reset
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: _hitungMBI,
+                    child: Text('Hitung BMI'),
+                  ),
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: _resetData,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                    ),
+                    child: Text('Reset'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 24),
+            
+            // Hasil
+            Card(
+              color: Colors.blue[50],
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Hasil BMI',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      _hasil?.toStringAsFixed(1) ?? '--',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      _kategori?.toUpperCase() ?? 'MASUKKAN DATA',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Jenis Kelamin: $_jenisKelamin',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:t3_santy/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('BMI Calculator smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CalculatorBMI());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the app bar title is 'Kalkulator BMI'.
+    expect(find.text('Kalkulator BMI'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that we have two text fields for berat and tinggi.
+    expect(find.byType(TextField), findsNWidgets(2));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that there is a button to calculate.
+    expect(find.text('Hitung BMI'), findsOneWidget);
   });
 }
